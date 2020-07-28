a29c7e5ff8602d8de531a866489e588e36203a83
nastassiamilashevskaya <52507944+nastassiamilashevskaya@users.noreply.github.com>
2020-07-28 23:36:07 +0300

    Updates

---
+++ precache-manifest.8bf13a3feea022195a35de2d40762f30.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b871f96c2f11696fe65a333178fe28f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "aa3197208e79733f003b",
+    "url": "/Portfolio/static/css/main.8b8ff1a7.chunk.css"
+  },
+  {
+    "revision": "421978a4df3e01b15024",
+    "url": "/Portfolio/static/js/2.f4fc31ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.f4fc31ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa3197208e79733f003b",
+    "url": "/Portfolio/static/js/main.e8be3490.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "eb978295a918105e0135dbcf49041e87",
+    "url": "/Portfolio/static/media/N.eb978295.svg"
+  },
+  {
+    "revision": "a51644c9b9540bf6c946804e13559d10",
+    "url": "/Portfolio/static/media/avatar.a51644c9.jpg"
+  },
+  {
+    "revision": "22b027cc3ac6ea243b09b0b004061b01",
+    "url": "/Portfolio/static/media/english-for-kids.22b027cc.gif"
+  },
+  {
+    "revision": "0973d24c4385f22a651c7af98cd1c745",
+    "url": "/Portfolio/static/media/movie-search.0973d24c.gif"
+  },
+  {
+    "revision": "c14c277f5254c56dbc4cda508279661c",
+    "url": "/Portfolio/static/media/rslang.c14c277f.gif"
+  },
+  {
+    "revision": "19878602b7772b23a6ee9ee53f4eb578",
+    "url": "/Portfolio/static/media/virtual-keyboard.19878602.gif"
+  },
+  {
+    "revision": "af77d88e7023c49eab71c3674fdcf43a",
+    "url": "/Portfolio/static/media/weather.af77d88e.gif"
+  }
+]);
