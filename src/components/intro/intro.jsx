88cb6eac6966b4d33e7d4cd8e11fd00dff824d73
nastassiamilashevskaya <52507944+nastassiamilashevskaya@users.noreply.github.com>
2020-07-29 18:32:31 +0300

    feat: add responsive

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -6,16 +6,19 @@ import './intro-animation.css'
 
 export default () => {
     const [inProp, setInProp] = useState(false);
+    const [width, setWidth] = useState(1200)
 
     useEffect(() => {
         setInProp(true)
+        setWidth(document.documentElement.clientWidth)
+        console.log(document.documentElement.clientWidth)
     }, [])
 
     return (
         <div className={styles.introContainer}>
             <div className={styles.introInner}>
                 <CSSTransition in={inProp} timeout={2000} classNames="name">
-                    <div className={styles.introName}>Hi, I'm <span><N /></span>astassia</div>
+                    <div className={styles.introName}>Hi, I'm{width < 700 ? <br /> : ' '}<span><N /></span>astassia</div>
                 </CSSTransition>
                 <CSSTransition in={inProp} timeout={2000} classNames="surname">
                     <div className={styles.introSurname}>front-end developer</div>
